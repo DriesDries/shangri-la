0da2c3a35aca043f6cdf4c0024e7a15a56b6df09
DriesDries <hystericglmaour7777@gmail.com>
2016-02-16 02:39:34 +0900

    add csv

--- anomaly_detection/anomaly.py
+++ anomaly_detection/anomaly.py
@@ -4,13 +4,22 @@
 Usage: $ python template.py <argv>
 ''' 
 
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+# 組み込み
 import os
 import sys
+import csv
 
-class name():
+# ライブラリ
+import cv2
+import numpy as np
+import numpy.random as rd
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from matplotlib import animation as ani
+import pandas as pd
+
+
+class AnomalyDetection():
 
     
     def __init__(self):
@@ -24,9 +33,31 @@ class name():
             dst  :      -> 
             param:      -> 
         '''
-        
-        out = img
-        return out
+
+        # 図の準備
+        nrow = 2
+        ncol = 2
+        plt.subplots(nrow, ncol, figsize=(16,7))
+        gs = gridspec.GridSpec(nrow,ncol)
+        axs = [plt.subplot(gs[i]) for i in range(4) ]
+
+        # 一つ目の図の描画 
+        rd.seed(0)
+        n = 141
+        x = np.linspace(0,140,n) # float64でつくる
+        y = rd.exponential(1.5, n) * 300 # 乱数じゃない
+        col = ["#2F79B0" for _ in range(n)] # colorの生成
+        for i in range(5):    
+            y[60+i] = rd.exponential(1.5, 1) * 300 + 2000
+            col[60+i] = "r"
+        axs[0].scatter(x,y, c=col) #散布図の描画
+        axs[0].set_xlim(-5,145)
+        axs[0].set_xlabel('time',size=12)
+
+        # plt.pause(3)
+        plt.show()
+
+
 
     def module2(self,img):
         '''
@@ -45,17 +76,18 @@ class name():
 ################################################################################
 if __name__ == '__main__':
 
-    cls = name()
+    ad = AnomalyDetection()
 
-    img = cv2.imread(sys.argv[1])
-    if img is None:
-        print  '!!!!!!There is not %s'%sys.argv[1] + '!!!!!!!!'
-        sys.exit()
-    out = cls.module1(img)
+    # img = cv2.imread(sys.argv[1])
+    # if img is None:
+        # print  '!!!!!!There is not %s'%sys.argv[1] + '!!!!!!!!'
+        # sys.exit()
+    # ad.module1(0)
 
-    
+    sam = pd.read_csv('tests.csv')
+    print sam
 
-    cv2.imshow('Input',  img)
-    cv2.imshow('Output', out)
+    # cv2.imshow('Input',  img)
+    # cv2.imshow('Output', out)
     cv2.waitKey(-1)
     
